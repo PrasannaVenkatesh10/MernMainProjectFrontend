88a6ab48c185b5249d935952e0e5d5779e614648
PV <717821p144@kce.ac.in>
2024-02-01 12:25:57 +0530

    config file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-slick'], // Add 'react-slick' to the external modules
+    },
+  },
 })
+
